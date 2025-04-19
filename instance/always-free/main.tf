af50745c69611f226fa4578bb97b07f177e5cf16
James Chen <camelinuk@users.noreply.github.com>
2025-04-20 02:53:49 +0800

    Update main.tf fingerprint

--- instance/always-free/main.tf
+++ instance/always-free/main.tf
@@ -9,10 +9,11 @@ variable "user_ocid" {
 }
 
 variable "fingerprint" {
+  default = "dd:46:ba:91:7d:8f:80:56:15:bc:5c:2a:de:01:0b:44"
 }
 
 variable "private_key_path" {
-  default = ""
+  default = "id_rsa"
 }
 
 variable "ssh_public_key" {
