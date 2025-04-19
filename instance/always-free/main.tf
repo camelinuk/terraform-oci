7467e1d4243b0ebb683179fc04cb9477ac4732ef
James Chen <camelinuk@users.noreply.github.com>
2025-04-20 00:25:45 +0800

    Update main.tf

--- instance/always-free/main.tf
+++ instance/always-free/main.tf
@@ -16,7 +16,7 @@ variable "private_key_path" {
 }
 
 variable "ssh_public_key" {
-  default = "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkkDoPXZg4oLfqYQ2PanS+QhaIN9dPSRskxyplK3teEjUjhriY2FnZ0NZBuuFhUm9XIKXCfZLQdWRZ4U3qAFcYQI8mvzR0xrba88e9A8gYm+j9VGkF59HOp4KBwBenKeJLDBCm0FbJ94+puVanVSKmgZa7K7boCflCSm4KshFl6/88peug9fhEpwGO6FCgvn1iH0JOasXGaC+IzJ6WCOMpjTdqWEVm0oVRPgp21odJwowRAix9V8kCOQ6Oy31wjSpXqW0nmp9Y1m3N+k6j0Yz40bVTPAL1+4s2TwuY6wh54incndNwbsppn9O7F1ZJP4moD+KO1JHCjuasgOyKYssL9e45GnPQFNIuVkCbe8LYHHK1gQe8/7Gt3GJF/i6vTUhuuRRr3gfLkyD/4WVUUSrEWjC1Dy42s91TdVlJNrROtjoNdOSVwMnQ34Uepxb4lLZYo48WlQPceoeg4pnuHUdaowBG5jly08JweUSbuJhBJo9gWo0DpMM0NAmL6/GgrrM="
+  default = ""
 }
 
 variable "compartment_ocid" {
@@ -363,7 +363,6 @@ data "oci_core_vnic" "app_vnic" {
 # See https://docs.oracle.com/iaas/images/
 data "oci_core_images" "test_images" {
   compartment_id           = var.compartment_ocid
-  #display_name             = "Ub20Free"
   operating_system         = "Oracle Linux"
   operating_system_version = "8"
   shape                    = var.instance_shape
